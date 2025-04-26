c72c75c81418589700f3ebed6ac4353532d18889
Developer <dev@example.com>
2025-04-26 01:08:34 -0400

    Remove shell scripts from git tracking and add gitignore

--- remove_search_form.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to remove search form from all HTML files
-echo "Removing search form from all HTML files..."
-
-# Use find to locate all HTML files and pipe to xargs to process them
-find . -name "*.html" -type f -print0 | xargs -0 -I{} sed -i '' '/<form method="get" class="searchform search-form"/,/<\/form>/d' {}
-
-echo "Done! Removed search form from HTML files." 
